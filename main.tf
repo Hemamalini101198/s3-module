4470d97b39ecaa22301a942a3c7e755744fd31bf
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2023-11-28 15:20:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.50.0"
+    }
+  }
+
+  #adding s3 bucket for remote state storage
+  backend "s3"{
+    bucket = "tf-statefiles-bucket"
+    key = "s3-buckets/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
 module "s3_bucket" {
     source = "./S3_module"
     s3_bucket_name = var.s3_bucket_name

28659f43e0e7920dfaeae71672c941f4ec2462b5
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2024-01-02 17:41:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
+module "s3"{
+    source = "./S3_module"
+    region = var.region
+    s3_bucket_name = var.s3_bucket_name
+}
 
+module "cloudfront"{
+    source = "./cloudfront"
+    origin_id = 
+    env = var.env
+    domain_name = var.domain_name
+    target_origin_id = var.target_origin_id
+    region = var.region
+    depends_on = [module.s3]    
+}

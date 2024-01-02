2b778219e8a2da4accb8ad9845c132de2a21520a
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2024-01-02 17:44:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ module "s3"{
 
 module "cloudfront"{
     source = "./cloudfront"
-    origin_id = 
+    origin_id = module.s3.s3_bucket_endpoint
     env = var.env
-    domain_name = var.domain_name
-    target_origin_id = var.target_origin_id
+    domain_name = module.s3.s3_bucket_endpoint
+    target_origin_id = module.s3.s3_bucket_endpoint
     region = var.region
     depends_on = [module.s3]    
 }

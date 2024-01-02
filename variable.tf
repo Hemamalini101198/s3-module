da318a6f7cea8db97a74c35c9222041bf2da7b9a
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2024-01-02 17:49:39 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,14 @@
+variable "region" {
+  description = "region where the s3 bucket will be created"
+  type = string  
+}
 
+variable "s3_bucket_name" {
+  description = "name of the s3 bucket"
+  type = string
+}
+
+variable "env" {
+  default = "environment of cloud front distribution"
+  type = string  
+}
